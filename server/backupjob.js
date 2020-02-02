f5362e3d90385cd3c7ceb25d2d40a0590299e45f
Matthias <dunkelberg.matthias@yahoo.de>
2020-02-02 18:53:50 +0100

    handling ftp errors

--- server/backupjob.js
+++ server/backupjob.js
@@ -4,30 +4,46 @@ var moment = require('moment')
 
 // daily client
 var c_d = new Client();
+c_d.on('error', function(err) {console.log(err);});
 c_d.on('ready', function() {
-  c_d.put('db.db', `/SolarBackups/aktuell.db`, function(err) {
-    if (err) throw err;
+  try {
+    c_d.put('db.db', `/SolarBackups/aktuell.db`, function(err) {
     c_d.end();
-  });
+  });    
+  }
+  catch (err) {
+    console.log(`Daily Client Error: ${err.message}`) 
+  }   
 });
 
 // monthly client
 var c_m = new Client();
+c_m.on('error', function(err) {console.log(err);});
 c_m.on('ready', function() {
-  c_m.put('db.db', `/SolarBackups/${moment().format('YYYYMMDD_kkmmss')}.db`, function(err) {
-    if (err) throw err;
+  try {
+    c_m.put('db.db', `/SolarBackups/${moment().format('YYYYMMDD_kkmmss')}.db`, function(err) {
     c_m.end();
   });
+  }
+  catch {
+    console.log(`Monthly Client Error: ${err.message}`)
+}
 });
 
 var CronJob = require('cron').CronJob;
 var job_daily = new CronJob('0 0 0 * * *', function() {
+  try {
     c_d.connect({host: 'fritz.box', user: 'solarlogger', password: 'einpasswort'});
+  }
+  catch (err) { console.log(`Daily Client Connction Error: ${err.message}`) }
 }, null, true);
 
 //safe monthly 
 var job_monthly = new CronJob('0 0 0 1 * *', function() {
-  c_m.connect({host: 'fritz.box', user: 'solarlogger', password: 'einpasswort'});
+  try {
+    c_m.connect({host: 'fritz.box', user: 'solarlogger', password: 'einpasswort'});
+  }
+  catch (err) { console.log(`Monthly Client Connction Error: ${err.message}`) }
 }, null, true);
 
 //job.start();
