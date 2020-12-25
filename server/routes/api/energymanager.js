f622c1e65ece0719f737cf73421183c52c70e56f
GitMatze <dunkelberg.matthias@yahoo.de>
2020-12-25 14:47:12 +0100

    add hysteresis

--- server/routes/api/energymanager.js
+++ server/routes/api/energymanager.js
@@ -27,7 +27,7 @@ getTargetTemp = function(hour, endtime) {
     }                
 }
 
-getThreshold = function(hour, endtime, temp, target_temp) {
+getThreshold = function(hour, endtime, temp, target_temp, is_heating) {
     timeDiff = endtime-hour
     tempDiff = target_temp-temp
     console.log(`timeDiff: ${timeDiff}`)
@@ -35,11 +35,15 @@ getThreshold = function(hour, endtime, temp, target_temp) {
 
     // if endtime has passed but temp is significantly lower than target temp, reduce threshold
     if (timeDiff<=0 && tempDiff > config.lowerThreshold.tempDiff) {
-        return config.lowerThreshold.threshold      
+        threshold = config.lowerThreshold.threshold      
     }
     else {
-       return config.threshold
-    }   
+       threshold = config.threshold
+    }
+    // hysteresis: lower threshold when heating is on
+    threshold = is_heating ? threshold-config.hysteresis : threshold
+    return threshold
+    
 }
 
 module.exports.controlWater = function(temp) {    
@@ -51,13 +55,13 @@ module.exports.controlWater = function(temp) {
     var hour = moment().hour()
     var endtime = config.endtime.month[moment().format("M")]
     var target_temp = getTargetTemp(hour, endtime)
-    var threshold = getThreshold(hour, endtime, temp, target_temp) // critical excess power to switch heating on
+    var threshold = getThreshold(hour, endtime, temp, target_temp, is_heating) // critical excess power to switch heating on
 
     var excess = is_heating ? -grid+1200 : -grid 
     var suf_power = excess > threshold
     var t_diff = moment().diff(moment(update_time), 'seconds') // time since grid was last updated
     if (t_diff >30) {
-        console.log('Grid power is not being updated, turn heat off')
+        console.log('Grid power is not being updated, turn heating off')
     }
     
     // 
